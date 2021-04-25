a67d53e052b86e7db51a21356b05c3355a523623
Ayi Hardiyanto <ayi.hardiyanto@aux.dkatalis.com>
2021-04-25 08:57:14 +0700

    [CHORES] change home route

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:summarator/presentation/screen/summarator/bloc/summarize_bloc.dart';
+import 'package:summarator/presentation/screen/summarator/summarator_screen.dart';
 import 'package:summarator/presentation/theme/theme_data.dart';
 
+import 'common/injections/injector.dart';
 import 'common/utils/screen_config.dart';
 
 class MyApp extends StatefulWidget {
@@ -31,7 +35,10 @@ class _MyAppState extends State<MyApp> {
           return MaterialApp(
             theme: themeData,
             debugShowCheckedModeBanner: false,
-            // home: SplashScreen(),
+            home: BlocProvider(
+              create: (context) => getIt<SummarizeBloc>(),
+              child: SummaratorScreen(),
+            ),
           );
         },
       ),
