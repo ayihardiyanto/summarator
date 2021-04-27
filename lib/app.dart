9e9bd8e67ea0a405b0f2fb473826395436a425dc
Ayi Hardiyanto <ayi.hardiyanto@aux.dkatalis.com>
2021-04-28 04:40:58 +0700

    [CHORES] add local datasource init

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:summarator/common/utils/logger.dart';
+import 'package:summarator/presentation/screen/summarator/bloc/activity_bloc.dart';
+import 'package:summarator/presentation/screen/summarator/bloc/history_bloc.dart';
 import 'package:summarator/presentation/screen/summarator/bloc/summarize_bloc.dart';
 import 'package:summarator/presentation/screen/summarator/summarator_screen.dart';
 import 'package:summarator/presentation/theme/theme_data.dart';
 
 import 'common/injections/injector.dart';
+import 'common/utils/database_utils.dart';
 import 'common/utils/screen_config.dart';
+
+Future<void> main() async {
+  await DatabaseUtil.initDatabase();
+  runZonedGuarded(() {
+    runApp(MyApp());
+  }, (Object error, StackTrace stackTrace) {
+    // Whenever an error occurs, call the `_reportError` function. This sends
+    // Dart errors to the dev console or Sentry depending on the environment.
+    Log.e('Execution Error: $error');
+    Log.e('Stack Trace: $stackTrace');
+  });
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -33,13 +51,22 @@ class _MyAppState extends State<MyApp> {
         builder: (context, constraints) {
           ScreenConfig.init(constraints);
           return MaterialApp(
-            theme: themeData,
-            debugShowCheckedModeBanner: false,
-            home: BlocProvider(
-              create: (context) => getIt<SummarizeBloc>(),
-              child: SummaratorScreen(),
-            ),
-          );
+              theme: themeData,
+              debugShowCheckedModeBanner: false,
+              home: MultiBlocProvider(
+                providers: [
+                  BlocProvider(
+                    create: (context) => getIt<SummarizeBloc>(),
+                  ),
+                  BlocProvider(
+                    create: (context) => getIt<ActivityBloc>(),
+                  ),
+                  BlocProvider(
+                    create: (context) => getIt<HistoryBloc>(),
+                  ),
+                ],
+                child: SummaratorScreen(),
+              ));
         },
       ),
     );
