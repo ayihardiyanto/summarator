f8f957177ea91225de957134a8c164397d2829a3
Ayi Hardiyanto <ayi.hardiyanto@aux.dkatalis.com>
2021-04-28 04:38:54 +0700

    [SCREEN] improve result box widget

--- lib/presentation/screen/summarator/widgets/result_box.dart
+++ lib/presentation/screen/summarator/widgets/result_box.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:summarator/common/utils/screen_config.dart';
+import 'package:summarator/domain/entities/summary_entity.dart';
+import 'package:summarator/presentation/screen/summarator/bloc/activity_bloc.dart';
 import 'package:summarator/presentation/screen/summarator/summarator_string.dart';
 import 'package:summarator/presentation/theme/color_theme.dart';
 import 'package:summarator/presentation/theme/text_styles.dart';
 
 class ResultBox extends StatelessWidget {
-  final String result;
+  final Summary result;
 
   const ResultBox({Key? key, required this.result}) : super(key: key);
   @override
@@ -30,19 +33,28 @@ class ResultBox extends StatelessWidget {
                   style: TextStyles.ttCommons
                       .copyWith(fontSize: 13, color: White.justWhite),
                 ),
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  icon: Icon(
-                    Icons.star_border,
-                    color: White.justWhite,
+                BlocBuilder<ActivityBloc, ActivityState>(
+                  builder: (context, state) => IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: Icon(
+                      state is FavoriteUpdated && state.favorite
+                          ? Icons.star
+                          : Icons.star_border,
+                      color: White.justWhite,
+                    ),
+                    onPressed: () {
+                      final bloc = BlocProvider.of<ActivityBloc>(context);
+                      if (state is FavoriteUpdated && state.favorite) {
+                        bloc.add(RemoveFavorite(summary: result));
+                      }
+                    },
                   ),
-                  onPressed: () {},
                 )
               ],
             ),
           ),
           Text(
-            result,
+            result.summarizedText!,
             style: TextStyles.ttCommons.copyWith(
               color: White.justWhite,
               fontSize: 18,
